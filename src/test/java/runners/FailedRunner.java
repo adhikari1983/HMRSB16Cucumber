6cb2072e674e29176a509e0ae22daa9c3917a2a7
adhikari1983 <91963837+adhikari1983@users.noreply.github.com>
2023-09-30 10:13:01 -0400

    failed runner class edited

--- src/test/java/runners/FailedRunner.java
+++ src/test/java/runners/FailedRunner.java
@@ -1,42 +1,14 @@
 package runners;
 
-//only for local execution purpose
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+//only for local execution purpose
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        //features we use-to provide the path of all the feature files or to one .feature file
         features = "@target/failed.txt",
-
-        //glue keyword we use-to provide the path of the package where all the step defs are available
-        glue = {"steps"},
-
         plugin = {"pretty"}
-        //when dryRun = true -> it stops actual execution, and then; it quickly scans all the steps def
-        // to provide the missing step definition
-        //when dryRun = false -> it executes the step defs
-        //dryRun = false,
-
-        //tags -> runs/executes the test case of our choice/selection
-        //tags = "@regression" -> at least one tag should be there
-        // tags = "@regression or @smoke" -> mostly used test EXECUTIONS in work environment
-        // tags = "@sprint1 or sprint2"   -> run with either one condition is true
-        // tags = "@sprint1 and sprint2"  -> boolean value should be true to run means both sprint needs to be there
-        //tags = "@login",  /** most of the time we play with tags & some time dryRun just 2get missing step*/
-
-        //it means, sometimes the console output for cucumber test is having some
-        //irrelevant information. But, when you set it to true, it removes all that
-        //irrelevant information from the console and will give you simple output
-        //monochrome = true,
-
-        //it used to print all the steps in console
-        // this report will be generated under target folder
-        //plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json",
-        //        "rerun:target/failed.txt"}
-
 )
 
 public class FailedRunner {
